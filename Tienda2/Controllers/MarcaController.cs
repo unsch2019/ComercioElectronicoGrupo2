a351e8d709d65fae37a54cc5755be50911852290
Richard Zapata <r_zapatac@hotmail.com>
2019-05-24 18:31:12 -0500

    Arquitectura 1.0

---
+++ Tienda2/Controllers/MarcaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Datos;
+
+namespace Tienda2.Controllers
+{
+    public class MarcaController : Controller
+    {
+        private ComercioEntities db = new ComercioEntities();
+
+        // GET: Marca
+        public ActionResult Index()
+        {
+            return View(db.Marca.ToList());
+        }
+
+        // GET: Marca/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // GET: Marca/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marca/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Denominacion")] Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Marca.Add(marca);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(marca);
+        }
+
+        // GET: Marca/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marca/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Denominacion")] Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(marca).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(marca);
+        }
+
+        // GET: Marca/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marca/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Marca marca = db.Marca.Find(id);
+            db.Marca.Remove(marca);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
